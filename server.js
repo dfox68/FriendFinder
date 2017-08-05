b500d4db146a37fa3c1439de332604712939509b
Donald Fox <dfox682000@gmail.com>
2017-08-04 22:55:00 -0500

    updated routing info

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 // Dependencies
 var express = require("express");
-var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
-
-
+var path = require("path");
+require('./app/routing/htmlRoutes')(app);
 // Create an instance of the express app.
 var app = express();
 
 // Specify the port.
 var port = 3000;
 
-// Set Handlebars as the default templating engine:
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-
 // Initiate the listener:
 app.listen(port);
+
+app.use(bodyParser.urlencoded({extended: false }));
+
+app.use(bodyParser.json());
